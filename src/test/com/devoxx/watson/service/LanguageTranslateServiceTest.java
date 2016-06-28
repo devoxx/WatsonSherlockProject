91db60e8503eb2348ad77990bfd48cfd0f2c0a63
Stephan Janssen <sja@bejug.org>
2016-06-28 20:16:22 +0200

    Added integration test

--- src/test/com/devoxx/watson/service/LanguageTranslateServiceTest.java
+++ src/test/com/devoxx/watson/service/LanguageTranslateServiceTest.java
@@ -12,7 +12,7 @@ public class LanguageTranslateServiceTest {
 
     // This is an integration test
     @Test
-    public void translateTest() {
+    public void translateEnglishToFrenchTest() {
 
         final LanguageTranslation languageTranslation = new LanguageTranslation();
         languageTranslation.setUsernameAndPassword("ae793894-f77b-4ed5-b76f-30247d1b6edb", "G5KQ65F7wo6p");
@@ -25,4 +25,21 @@ public class LanguageTranslateServiceTest {
         Assert.notNull(translate);
         Assert.isTrue("C'est un test".equalsIgnoreCase(translate));
     }
+
+    // This is an integration test
+    @Test
+    public void translateFrenchToEnglishTest() {
+
+        final LanguageTranslation languageTranslation = new LanguageTranslation();
+        languageTranslation.setUsernameAndPassword("ae793894-f77b-4ed5-b76f-30247d1b6edb", "G5KQ65F7wo6p");
+
+        final LanguageTranslateService languageTranslateService = new LanguageTranslateService();
+        languageTranslateService.setLanguageTranslation(languageTranslation);
+
+        final String translate = languageTranslateService.translate("C'est un test", Language.FRENCH, Language.ENGLISH);
+
+        Assert.notNull(translate);
+        Assert.isTrue("it is a test".equalsIgnoreCase(translate));
+    }
+
 }
